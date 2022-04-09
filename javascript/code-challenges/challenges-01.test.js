f846c522df00d2438c275fd5f51494efaaec54d1
Ahmad-Khaled-Zaid <ahmadk.zaid97@gmail.com>
2022-04-09 17:42:30 +0300

    solve the stretch goal

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -94,6 +94,7 @@ const addNumbers = (num, arr, times, callback) => {
   for (let i = 0; i < times; i++) {
     callback(arr, num);
   }
+  console.log(arr);
   return arr;
   // Solution code here...
 };
@@ -229,7 +230,7 @@ describe("Testing challenge 6", () => {
   });
 });
 
-xdescribe("Testing challenge 7", () => {
+describe("Testing challenge 7", () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test("It should print out messages or numbers", () => {
